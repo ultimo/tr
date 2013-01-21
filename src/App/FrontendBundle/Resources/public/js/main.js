a8096f8b0109ccddad56532a3d1142bcc4ef671f
TR <tr@trs-mbp-2.local>
2013-01-21 10:21:48 +0100

    some js goodies
    Signed-off-by: TR <tr@trs-mbp-2.local>

--- src/App/FrontendBundle/Resources/public/js/main.js
+++ src/App/FrontendBundle/Resources/public/js/main.js
@@ -1,1 +1,17 @@
+$('#home').animate({'height':$(window).height()-60}, 0, function() {
+	$('.page-header').animate({'opacity':1}, 500);
+});
 
+$('#toolbelt li').popover({placement: 'bottom', trigger: 'click'}).click(function() {
+	$(this).siblings().popover('hide');
+});
+
+$('#skills .bar').width(0);
+
+$('#skills').waypoint(function(direction) {
+	if (direction == 'down') {
+		$('#skills .bar').width(function(){
+			$(this).width($(this).attr('data-width')+'%');
+		});
+	}
+}, {offset: '70%'});
