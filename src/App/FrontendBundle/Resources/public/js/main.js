4140c537fc5cb4637f5665706860f91d66aa6442
TR <tr@trs-mbp-2.local>
2013-02-01 17:18:25 +0100

    splitting js files into single sources, will be combined by yui for prod env
    Signed-off-by: TR <tr@trs-mbp-2.local>

--- src/App/FrontendBundle/Resources/public/js/main.js
+++ src/App/FrontendBundle/Resources/public/js/main.js
@@ -1,19 +1,31 @@
-$('#home').animate({'height':$(window).height()-60}, 0, function() {
-	$('.page-header').animate({'opacity':1}, 500);
-});
-$('.submit').addClass('btn');
-$('.submit').click(function(){
-	window.location.hash = '#contact';
-});
-$('#toolbelt li').popover({placement: 'bottom', trigger: 'click'}).click(function() {
-	$(this).siblings().popover('hide');
-});
-$('#skills .bar').width(0);
-
-$('#skills').waypoint(function(direction) {
-	if (direction == 'down') {
-		$('#skills .bar').width(function(){
-			$(this).width($(this).attr('data-width')+'%');
+$(document).ready(function () {
+	var setHomeHeight = function (animateHeader) {
+		$('#home').animate({'height':$(window).height() - 60}, 0, function() {
+			if (animateHeader) {
+				$('.page-header').animate({'opacity':1}, 800);
+			}
 		});
-	}
-}, {offset: '50%'});
+	};
+
+	setHomeHeight(true);
+    
+	$(window).resize(function (argument) {
+		setHomeHeight(false);
+	});
+
+	$('nav a').smoothScroll();
+
+	$('#toolbelt li').popover({placement: 'bottom', trigger: 'click'}).click(function() {
+		$(this).siblings().popover('hide');
+	});
+
+	$('#skills .bar').width(0);
+
+	$('#skills').waypoint(function(direction) {
+		if (direction == 'down') {
+			$('#skills .bar').width(function(){
+				$(this).width($(this).attr('data-width')+'%');
+			});
+		}
+	}, {offset: '50%'});
+});
